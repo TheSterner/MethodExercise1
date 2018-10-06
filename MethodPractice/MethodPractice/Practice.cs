deaf8348376461a26489aae2546b126c032f008d
William B McAllister <blakemcallister77@gmail.com>
2018-10-06 19:40:25 -0400

    z

---
+++ MethodPractice/MethodPractice/Practice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPractice
+{
+	public class Practice
+	{
+		public void Integers(int j, int k)
+		{
+			k = k + 10;
+			Console.WriteLine(j);
+			
+		} 
+		
+	}
+}
---
+++ MethodPractice/MethodPractice/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPractice
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Practice practice = new Practice();
+			Console.WriteLine("Please pick a number between 1-50.");
+			int userNumber = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Please pick another number between 1-50");
+			int userNumber2 = Convert.ToInt32(Console.ReadLine());
+			practice.Integers(userNumber, userNumber2);
+			
+
+		}
+		
+
+		
+	}
+}
